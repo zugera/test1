8d35c37b719f29d715ab1f85c48b51636ea9dd82
CHEON <CHEON@CHEON-PC>
2014-06-25 02:21:36 +0900

    Test.java 파일 추가

---
+++ honeyskin/src/kr/co/sist/action/Test.java
@@ -0,0 +1,24 @@
+package kr.co.sist.action;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Test extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public Test() {
+		System.out.println("test¼º°ø");
+	}
+
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+	}
+
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+	}
+
+}
